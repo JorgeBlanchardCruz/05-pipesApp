78efbb9d75cae202e057cc3da26ed221a95c944c
Jorge Blanchard Cruz <rothgard@live.com>
2024-04-22 16:27:50 +0100

    next

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -30,5 +31,28 @@ export class UncommonPageComponent {
   public popClient(): void {
     this.clients.pop();
   }
+
+
+  //Key-Value Pipe
+  public person = {
+    name: 'George',
+    age: 41,
+    address: 'Ottawa, Canada',
+  }
+
+
+  //Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap(() => console.log('Timer executed')));
+
+  // las promesas no se pueden cancelar
+  public myPromiseValue: Promise<string>  = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('tenemos data en la promesa');
+      console.log('Promise executed');
+      this.person.name = 'Alice';
+    }, 3500);
+  });
+
 
 }
